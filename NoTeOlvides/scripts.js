243281af511803ea98b9ec12ba074c6f63b8146f
ACaminos <caminosariel@gmail.com>
2020-07-10 18:53:15 -0300

    cambios

---
+++ NoTeOlvides/scripts.js
@@ -0,0 +1,8 @@
+const RecordatorioGrabado = document.getElementById('RecordatoriosGrabados');
+
+eventListeners();
+
+function eventListeners(){
+    //Cuando se envia el formulario
+    document.querySelector('Formulario');
+}
