6326768ab081e3ff5a372b7f1a158d7a6bf09037
ACaminos <caminosariel@gmail.com>
2020-07-11 21:48:52 -0300

    con funcionalidad parcial

--- NoTeOlvides/scripts.js
+++ NoTeOlvides/scripts.js
@@ -1,12 +1,31 @@
-const RecordatorioGrabado = document.getElementById('RecordatoriosGrabados');
+const RecordatorioGrabado = document.getElementById('Final');
 
 eventListeners();
 
 function eventListeners(){
     //Cuando se envia el formulario
-    document.querySelector('Formulario').addEventListener('submit',agregarRecordatorio);
+    document.querySelector('#Formulario').addEventListener('submit',agregarRecordatorio);
 }
 
 function agregarRecordatorio(e){
-    e.pr
+    e.preventDefault();
+    const Titulo = document.querySelector('#Title').value;
+    const Descripcion = document.querySelector('#comentario').value;
+    let eleccion = document.getElementsByName('importancia');
+    for(i=0; i<eleccion.length; i++){
+        if(eleccion[i].checked){
+        eleccion =eleccion[i].value;
+        }
+    }
+    const botonBorrar = document.createElement('a'); //Intento de boton de borrar sin funcionalidad
+    botonBorrar.classList = 'borrar-recordatorio';
+    botonBorrar.innerText= 'X';
+    const objeto = {
+        Titulo , Descripcion , eleccion
+    }
+
+    const lista = document.getElementById('Final');
+    lista.innerHTML = " Titulo: " + objeto.Titulo+"\n"+ " Descripción: " + objeto.Descripcion +"\n"+ " Prioridad: " +objeto.eleccion + "\n";
+     
+    console.log(objeto);
 }
